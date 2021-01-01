fc3354147e5f0fe4213446d281dcb64223979060
Syed Zaki <56019676+syedzaki114689@users.noreply.github.com>
2021-01-01 15:51:22 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 from math import trunc
 import pygame
 import random
+from pygame import *
+import math
 
 # globals
-display_width = 400
-display_height = 400
-population = 100
+display_width = 800
+display_height = 800
+population = 1000000
+personperrow = math.sqrt(population)
+print(personperrow)
+width = display_width/personperrow
+height = display_height/personperrow
+margin = ((width/100)*1.5, (height/100)*1.5)
+infection_percent = 0.002
 
 
 # predefined colours
@@ -33,27 +41,139 @@ class person():
 def move_person(people):
     for persons in people:
         i = 0
-        displacement = random.random()
+
         while(True):
-            if(displacement < 0.2):
+            displacement = random.random()
+
+            if(displacement < 0.1):
                 break
 
+        a = random.random()
+
+        if(a < 0.5):
+            displacement = displacement * -1
+
         x, y = persons.location
-        x += displacement
-        y += displacement
+        if(x <= 0 or x >= display_width):
+            if(displacement < 0 and x >= display_width+width):
+                x += displacement
+            if(displacement > 0 and x <= 0):
+                x += displacement
+
+        else:
+            x += displacement
+        if(y <= 0 or x >= display_height):
+            if(displacement < 0 and y >= display_height+height):
+                y += displacement
+            if(displacement > 0 and y <= 0):
+                y += displacement
+
+        else:
+            y += displacement
         persons.location = (x, y)
+
+
+def display_person(people):
+    for p in people:
+        if(p.infected == True):
+            pygame.draw.rect(
+                dis, red, (((p.location[0] * width)), ((p.location[1]*height)), width-margin[0], height-margin[1]))
+        else:
+            pygame.draw.rect(
+                dis, black, (((p.location[0] * width)), ((p.location[1]*height)), width-margin[0], height-margin[1]))
+
+
+def event_handler():
+
+    for event in pygame.event.get():
+        if (event.type == QUIT):
+            running = False
+            pygame.quit()
+            quit()
+
+
+def check_spread(people):
+    for p in people:
+
+        if(p.infected == True):
+
+            x, y = p.location
+            if(x > 0):
+                for q in people:
+                    if(q.location == (x-1, y)):
+                        chance = random.random()
+                        if(chance < infection_percent):
+                            # print("111")
+                            q.infected = True
+
+            if(x < personperrow-1):
+                for q in people:
+                    if(q.location == (x+1, y)):
+                        chance = random.random()
+                        if(chance < infection_percent):
+                            q.infected = True
+
+            if(y > 0):
+                for q in people:
+                    if(q.location == (x, y-1)):
+                        chance = random.random()
+                        if(chance < infection_percent):
+                            q.infected = True
+
+            if(y < personperrow-1):
+                for q in people:
+                    if(q.location == (x, y+1)):
+                        chance = random.random()
+                        if(chance < infection_percent):
+                            q.infected = True
+
+    return people
+
+
+def check_end_state(people):
+    c = False
+    for p in people:
+        if(p.infected == False):
+            c = True
+    return c
 
 
 def game_loop():
     people = []
+    j = 0
+    k = 0
     for i in range(population):
-        people.append(person(i, (0, 0)))
+        people.append(person(i, (j, k)))
+        #print(j, "  ", k)
 
-    while(True):
-        move_person(people)
+        if(j >= personperrow-1):
+            j = 0
+            k += 1
+        else:
+            j += 1
+
+    center = math.ceil(personperrow/2)
+
+    for p in people:
+        if(p.location == (center, center)):
+            p.infected = True
+
+    print()
+
+    while(check_end_state(people)):
+        event_handler()
+        display_person(people)
+        # move_person(people)
+        people = check_spread(people)
         pygame.display.update()
         dis.fill(white)
         pygame.time.wait(50)
-        print("Hasnain")
-        print("Zaki")
 
+        """"if(a > 100):
+            break
+        else:
+            a += 1
+"""""
+
+
+game_loop()
